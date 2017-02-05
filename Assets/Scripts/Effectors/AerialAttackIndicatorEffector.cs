b5aa4b5dc8e486f5385e208d7e1f8cf97228cae1
jannef <janne.forsell@cs.tamk.fi>
2017-02-05 02:20:39 +0200

    * Changed some variable names to be in accordance with ReSharper recommendation.

--- Assets/Scripts/Effectors/AerialAttackIndicatorEffector.cs
+++ Assets/Scripts/Effectors/AerialAttackIndicatorEffector.cs
@@ -15,7 +15,7 @@ namespace fi.tamk.hellgame.effectors
 
         protected AirDropInitializer AirDropInitializer = null;
 
-        private GameObject EffectGameObject;
+        private GameObject _effectGameObject;
         private Projector _landindProjector;
         private float _time = 0f;
 
@@ -23,19 +23,21 @@ namespace fi.tamk.hellgame.effectors
         {
             base.Activate();
             AirDropInitializer = GetComponent<AirDropInitializer>();
-            EffectGameObject = Instantiate(ProjectorPrefab, Effect.transform);
-            EffectGameObject.transform.localPosition = Vector3.zero;
-            _landindProjector = EffectGameObject.GetComponent<Projector>();
+            _effectGameObject = Instantiate(ProjectorPrefab, Effect.transform);
+            _effectGameObject.transform.localPosition = Vector3.zero;
+            _landindProjector = _effectGameObject.GetComponent<Projector>();
 
+            /*
             var droppingDamageCollider = GetComponentInChildren<DroppingDamageCollider>();
 
             if (droppingDamageCollider != null)
             {
                 Effect.SetOnEnd(droppingDamageCollider.SelfDestruct, new float[0]);
             }
+            */
 
             Effect.SetOnUpdateCycle(FollowAnother, new float[]{0});
-            Effect.SetOnEnd(ScreenShakeEffect, new float[2] { ImpactScreenShakeIntensity, ImpactScreenShakeLenght });
+            Effect.SetOnEnd(ScreenShakeEffect, new float[] { ImpactScreenShakeIntensity, ImpactScreenShakeLenght });
 
             if (AirDropInitializer == null) return;
             Effect.LifeTime = AirDropInitializer.FallingDuration;
@@ -44,7 +46,7 @@ namespace fi.tamk.hellgame.effectors
         public void FollowAnother(float[] args)
         {
             Effect.transform.position = AirDropInitializer.transform.position;
-            EffectGameObject.transform.forward = AirDropInitializer.LandingCoordinates.position - transform.position;
+            _effectGameObject.transform.forward = AirDropInitializer.LandingCoordinates.position - transform.position;
             _time += Time.deltaTime / Effect.LifeTime;
             _landindProjector.orthographicSize = Mathf.Lerp(ProjectionMinsize, ProjectionMaxSize, AirDropInitializer.FallingCurve.Evaluate(_time));
         }
