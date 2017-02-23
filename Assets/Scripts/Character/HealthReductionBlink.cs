cf097f07c572cd12845f781937dd7a942dc4cff0
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-02-23 12:59:53 +0200

    + New colours

---
+++ Assets/Scripts/Character/HealthReductionBlink.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.character
+{
+
+    public class HealthReductionBlink : MonoBehaviour
+    {
+
+
+    }
+}
