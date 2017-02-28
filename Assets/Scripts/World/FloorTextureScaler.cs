4c8cb7218b2bdedc52f43da64f0bd06af398d82c
jannef <janne.forsell@cs.tamk.fi>
2017-02-28 09:31:27 +0200

    +Floor texture scaler: despite it's name it sets the tiling for a texture based on the local scale of the object it is in. Name comes from unity method used to change tiling Material::mainTextureScale).

---
+++ Assets/Scripts/World/FloorTextureScaler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace fi.tamk.hellgame.world
+{
+    public class FloorTextureScaler : MonoBehaviour
+    {
+        [Range(0.1f, 10f), SerializeField] private float _tileMultiplierX = 1f;
+        [Range(0.1f, 10f), SerializeField] private float _tileMultiplierZ = 1f;
+        [SerializeField] private bool _useXForBoth = false;
+
+        private void Awake()
+        {
+            var material = gameObject.GetComponent<Renderer>().material;
+            material.mainTextureScale = new Vector2(gameObject.transform.localScale.x * _tileMultiplierX, gameObject.transform.localScale.z * (_useXForBoth ? _tileMultiplierX :_tileMultiplierZ));
+        }
+    }
+}
