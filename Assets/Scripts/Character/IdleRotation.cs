b3161640bb4d4611e06c0fef536de328a3999616
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-03-17 10:11:06 +0200

    * Tested a slimeboss minion death particle effect that is less overpowering + Very simple idle rotation script

---
+++ Assets/Scripts/Character/IdleRotation.cs
@@ -0,0 +1,21 @@
+﻿using fi.tamk.hellgame.world;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.character
+{
+
+    public class IdleRotation : MonoBehaviour
+    {
+
+        [SerializeField] private float _rotationSpeed;
+        [SerializeField] private Vector3 _rotationDirection;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(_rotationDirection.normalized * _rotationSpeed * WorldStateMachine.Instance.DeltaTime);
+        }
+    }
+}
