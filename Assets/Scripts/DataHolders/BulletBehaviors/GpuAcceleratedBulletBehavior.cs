fc6fbe8ecca734e1feafba7260b506890467bc35
Janne Forsell <jannef1337@gmail.com>
2017-03-19 20:13:49 +0200

    + gpu based bullet behavior calculations: To use gpu behaviors you need compute shader and a ScriptableObject derived from GpuAcceleratedBulletBehavior to drive it. + Example of gpu bullet behavior, accelerating bullet that has configurable m/s^2 acceleration value. See AcceleratingBullet.compute and GpuBulletAcceleration.cs for the example.

---
+++ Assets/Scripts/DataHolders/BulletBehaviors/GpuAcceleratedBulletBehavior.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace fi.tamk.hellgame.dataholders
+{
+    public abstract class GpuAcceleratedBulletBehavior : AdvancedBulletBehavior
+    {
+        [SerializeField] protected ComputeShader BehaviorComputeShader;
+        protected ComputeBuffer CsBuffer;
+
+        public abstract override void BatchedAction(ref ParticleSystem.Particle[] particleBuffer, int numberOfParticles);
+
+        public override void Action(ref ParticleSystem.Particle particle)
+        {
+            throw new UnityException("GpuAcceleratedBulletBehavior can't give out per particle data.");
+        }
+
+        public abstract override void InitializeBatch(int maxBatchSize);
+
+        protected void InternalBatchedAction<T>(ref T[] inBuffer, ref ParticleSystem.Particle[] particleBuffer, int numberOfParticles)
+            where T : struct
+        {
+            var kernelIndex = BehaviorComputeShader.FindKernel("CSMain");
+            uint x, y, z;
+            BehaviorComputeShader.GetKernelThreadGroupSizes(kernelIndex, out x, out y, out z);
+
+            DataToShader(ref inBuffer, ref particleBuffer, numberOfParticles);
+            CsBuffer.SetData(inBuffer);
+            BehaviorComputeShader.SetBuffer(kernelIndex, "dataBuffer", CsBuffer);
+
+            BehaviorComputeShader.Dispatch(kernelIndex, numberOfParticles / (int)z + 1, 1, 1);
+
+            CsBuffer.GetData(inBuffer);
+            ShaderToData(ref inBuffer, ref particleBuffer, numberOfParticles);
+        }
+
+        protected abstract void DataToShader<T>(ref T[] inBuffer, ref ParticleSystem.Particle[] particles, int numberOfParticles) where T : struct;
+        protected abstract void ShaderToData<T>(ref T[] inBuffer, ref ParticleSystem.Particle[] particles, int numberOfParticles) where T : struct;
+
+        public override void ReleaseResources()
+        {
+            base.ReleaseResources();
+            if (CsBuffer != null) CsBuffer.Dispose();
+        }
+    }
+}
---
+++ Assets/Scripts/DataHolders/BulletBehaviors/GpuBulletAcceleration.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using fi.tamk.hellgame.world;
+
+namespace fi.tamk.hellgame.dataholders
+{
+    public sealed class GpuBulletAcceleration : GpuAcceleratedBulletBehavior
+    {
+        [SerializeField] private float _accelerationAmounth = 0f;
+
+        private struct BufferDataType
+        {
+            public Vector3 velocity;
+        }
+
+        private BufferDataType[] _inBuffer;
+
+        protected override void DataToShader<T>(ref T[] inBuffer, ref ParticleSystem.Particle[] particles,
+            int numberOfParticles)
+        {
+            BehaviorComputeShader.SetFloat("accelerate", _accelerationAmounth);
+            BehaviorComputeShader.SetFloat("delta", WorldStateMachine.Instance.DeltaTime);
+            for (var i = 0; i < numberOfParticles; ++i)
+            {
+                _inBuffer[i].velocity = particles[i].velocity;
+            }
+        }
+
+        protected override void ShaderToData<T>(ref T[] inBuffer, ref ParticleSystem.Particle[] particles,
+            int numberOfParticles)
+        {
+            for (var i = 0; i < numberOfParticles; ++i)
+            {
+                particles[i].velocity = _inBuffer[i].velocity;
+            }
+        }
+
+        public override void InitializeBatch(int maxBatchSize)
+        {
+            CsBuffer = new ComputeBuffer(maxBatchSize, 12); // one vector3 makes 12 bytes
+            _inBuffer = new BufferDataType[maxBatchSize];
+        }
+
+        public override void BatchedAction(ref ParticleSystem.Particle[] particleBuffer, int numberOfParticles)
+        {
+            InternalBatchedAction<BufferDataType>(ref _inBuffer, ref particleBuffer, numberOfParticles);
+        }
+    }
+}
