eec167b36ad181d6811fca7bbb78cc45812f034f
Janne Forsell <jannef1337@gmail.com>
2017-03-23 11:46:57 +0200

    + Courtyard boss started

---
+++ Assets/Scripts/States/Bosses/Courtyard/CourtyardBase.cs
@@ -0,0 +1,15 @@
+﻿using fi.tamk.hellgame.character;
+using fi.tamk.hellgame.states;
+using UnityEngine;
+
+public abstract class CourtyardBase : StateAbstract {
+
+    protected Transform GunTarget;
+    protected BossExternalObjects Externals;
+
+    public CourtyardBase(ActorComponent controlledHero) : base(controlledHero)
+    {
+
+    }
+
+}
