864adb4dd0157d1cb190321704b4f05da58c98d0
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-03-31 13:59:37 +0300

    + FMOD integration into the project. Includes the FMOD studio project, and plugin which integrates the fmod sound bank into the Unity project

---
+++ Assets/Scripts/Effects/OneShotSoundEffect.cs
@@ -0,0 +1,34 @@
+﻿using FMODUnity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.effects
+{
+
+    public class OneShotSoundEffect : MonoBehaviour
+    {
+        [EventRef]
+        public String Event = "";
+
+        public void Play()
+        {
+            if (String.IsNullOrEmpty(Event)) return;
+
+            FMODUnity.RuntimeManager.PlayOneShot(Event, transform.position);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
