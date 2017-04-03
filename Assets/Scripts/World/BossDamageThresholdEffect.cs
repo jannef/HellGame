0425cbd1ea772062cd799035ab9266af6cc091d9
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-04-03 12:20:39 +0300

    * Added health bar to WallBoss

---
+++ Assets/Scripts/World/BossDamageThresholdEffect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace fi.tamk.hellgame.world
+{
+    public class BossDamageThresholdEffect : MonoBehaviour
+    {
+        private float HealthPercentage;
+        [SerializeField] private float TotalAmountOfEffects;
+        [SerializeField] private float OrderNumber;
+        public UnityEvent ActivationEvent;
+        private bool hasActivated = false;
+        private BossDamageThresholdEffectMaster _master;
+
+        private void Start()
+        {
+            _master = GetComponentInParent<BossDamageThresholdEffectMaster>();
+            _master.DamageEvent += OnDamageTaken;
+            HealthPercentage = 1f - ((1f / TotalAmountOfEffects) * OrderNumber);
+        }
+
+        private void OnDamageTaken(float percentage)
+        {
+            if (!hasActivated && percentage <= HealthPercentage)
+            {
+                hasActivated = true;
+                if (ActivationEvent != null)
+                {
+                    ActivationEvent.Invoke();
+                }
+
+                if (_master != null) _master.DamageEvent -= OnDamageTaken;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/World/BossDamageThresholdEffectMaster.cs
@@ -0,0 +1,32 @@
+﻿using fi.tamk.hellgame.character;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.world
+{
+
+    public class BossDamageThresholdEffectMaster : MonoBehaviour
+    {
+        public event Action<float> DamageEvent;
+        [SerializeField] private HealthComponent _bossSystemToFollow;
+
+        private void Start()
+        {
+            if (_bossSystemToFollow != null) _bossSystemToFollow.HealthChangeEvent += TakeDamage;
+            _bossSystemToFollow.DeathEffect.AddListener(DeathEffect);
+        }
+
+        private void DeathEffect()
+        {
+            TakeDamage(0f, 0, 0);
+        }
+
+        // Use this for initialization
+        void TakeDamage(float percentage, int hp, int max)
+        {
+            if (DamageEvent != null) DamageEvent.Invoke(percentage);
+        }
+    }
+}
