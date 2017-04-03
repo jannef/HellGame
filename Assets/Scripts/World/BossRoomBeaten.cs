0255db81b62a2891801d4afc76415adefcd29abd
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-04-03 10:41:58 +0300

    * Added exits to Cellar_Boss, a simple script activates the room beaten event

---
+++ Assets/Scripts/World/BossRoomBeaten.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.world
+{
+
+    public class BossRoomBeaten : MonoBehaviour
+    {
+
+        public void RoomHasBeenBeaten()
+        {
+            RoomIdentifier.OnRoomCompleted();
+        }
+    }
+}
