d51ac06c1af1aea0437304de6b8e6badf7908aed
Janne Forsell <jannef1337@gmail.com>
2017-04-03 14:37:39 +0300

    Work on courtyard boss
    + Homing sun on courtyard
    + Homing sun pulses with a heartbeat
    + Effects to courtyard's fire hazard thing
    + Pickups to courtyard boss
    + Angry shake effect to courtyard boss

---
+++ Assets/Scripts/Effects/AngryShakeEffect.cs
@@ -0,0 +1,35 @@
+﻿using fi.tamk.hellgame.world;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.effects
+{
+    public class AngryShakeEffect : MonoBehaviour
+    {
+        [Range(0.01f, 10f), SerializeField] private float _duration = 5f;
+        [Range(0.01f, 10f), SerializeField] private float _amounth = 0.2f;
+
+        public void Activate()
+        {
+
+        }
+
+        private IEnumerator ShakeDat(float duration)
+        {
+            Vector3 original = transform.position;
+            var timer = 0f;
+
+            while (timer < duration)
+            {
+                var offset = Random.insideUnitSphere * _amounth;
+                offset.y = 0;
+
+                timer += WorldStateMachine.Instance.DeltaTime;
+                yield return null;
+            }
+
+            transform.position = original;
+        }
+    }
+}
