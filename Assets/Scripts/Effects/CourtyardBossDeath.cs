5af8ffeb2f9d58c5a0cdecd5dcfee5759b27392b
Janne Forsell <jannef1337@gmail.com>
2017-04-05 15:03:49 +0300

    Courtyard boss death upgrade
    + Death phase for the boss
    + Death trigger for the boss

---
+++ Assets/Scripts/Effects/CourtyardBossDeath.cs
@@ -0,0 +1,26 @@
+﻿using fi.tamk.hellgame.character;
+using fi.tamk.hellgame.states;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.effects
+{
+    public class CourtyardBossDeath : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _particles;
+        private ActorComponent _actor;
+
+        public void Awake()
+        {
+            _actor = GetComponent<ActorComponent>() ?? new UnityException("CourtyardBossDeath MonobBehaviour component" +
+                "can't find reference to ActorComponent in GO: " + gameObject).Throw<ActorComponent>();
+        }
+
+        public void Activate()
+        {
+            if (_particles != null) _particles.Play();
+            _actor.GoToState(new CourtyardDeathPhase(_actor, _actor.CurrentState as CourtyardBase));
+        }
+    }
+}
---
+++ Assets/Scripts/States/Bosses/Courtyard/CourtyardDeathPhase.cs
@@ -0,0 +1,17 @@
+﻿
+using fi.tamk.hellgame.character;
+
+namespace fi.tamk.hellgame.states
+{
+    public class CourtyardDeathPhase : CourtyardBase
+    {
+        public CourtyardDeathPhase(ActorComponent controlledHero, CourtyardBase clonedState = null) : base(controlledHero, clonedState)
+        {
+        }
+
+        public override void HandleInput(float deltaTime)
+        {
+            base.HandleInput(deltaTime);
+        }
+    }
+}
