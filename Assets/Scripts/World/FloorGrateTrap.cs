bb97da039829c21996df1350271fbdf840783940
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-04-10 22:52:40 +0300

    + Simple floor traps to test them out

---
+++ Assets/Scripts/World/FloorGrateTrap.cs
@@ -0,0 +1,76 @@
+﻿using fi.tamk.hellgame.character;
+using fi.tamk.hellgame.utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace fi.tamk.hellgame.world
+{
+
+    public class FloorGrateTrap : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem _telegraphParticles;
+        [SerializeField] private ParticleSystem _mainFlameParticles;
+        [SerializeField] private DamagingCollider _damagingCollider;
+        [SerializeField] private float _telegraphLength;
+        [SerializeField] private float _flameActiveLenght;
+        [SerializeField] private float _coolDownLenght;
+        [SerializeField] private float _startCoolDown;
+
+        public UnityEvent StartFlame;
+
+        private bool running = false;
+
+        public void Activate()
+        {
+            running = true;
+            StartCoroutine(StaticCoroutines.DoAfterDelay(_coolDownLenght - _startCoolDown, StartTelegraph));
+        }
+
+        private void StartTelegraph()
+        {
+            if (!running)
+            {
+                Stop();
+                return;
+            }
+
+            _telegraphParticles.Play();
+
+            StartCoroutine(StaticCoroutines.DoAfterDelay(_telegraphLength, StartFlames));
+        }
+
+        private void StartFlames()
+        {
+            if (!running)
+            {
+                Stop();
+                return;
+            }
+
+            if (StartFlame != null) StartFlame.Invoke();
+            _telegraphParticles.Stop();
+            _mainFlameParticles.Play();
+            _damagingCollider.enabled = true;
+
+            StartCoroutine(StaticCoroutines.DoAfterDelay(_flameActiveLenght, StopFlames));
+        }
+
+        private void StopFlames()
+        {
+            _damagingCollider.enabled = true;
+            _mainFlameParticles.Stop();
+            StartCoroutine(StaticCoroutines.DoAfterDelay(_coolDownLenght, StartTelegraph));
+        }
+
+        public void Stop()
+        {
+            _damagingCollider.enabled = false;
+            _mainFlameParticles.Stop();
+            _telegraphParticles.Stop();
+            running = false;
+            StopAllCoroutines();
+        }
+    }
+}
---
+++ Assets/Scripts/World/LaserTrap.cs
@@ -0,0 +1,94 @@
+﻿using fi.tamk.hellgame.dataholders;
+using fi.tamk.hellgame.utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace fi.tamk.hellgame.world
+{
+
+    public class LaserTrap : MonoBehaviour
+    {
+        public UnityEvent startLaser;
+        public UnityEvent stopLaser;
+        public UnityEvent deathEvent;
+        [SerializeField] private Transform _startingPoint;
+        [SerializeField] private Transform _secondPoint;
+        [SerializeField] private WallBossMovement _movementStats;
+        [SerializeField] private float _cooldown;
+        [SerializeField] private LaserEmitter _emitter;
+
+        private bool _isInStartingPosition = true;
+        private Action _stopLaserAction;
+
+        void Start()
+        {
+            Activate();
+        }
+
+        public void Stop()
+        {
+            if (_stopLaserAction != null) _stopLaserAction.Invoke();
+            if (deathEvent != null) deathEvent.Invoke();
+            
+        }
+
+        public void Activate()
+        {
+            StartCoroutine(StaticCoroutines.DoAfterDelay(_cooldown, StartAttack));
+        }
+
+        private void StartAttack()
+        {
+
+            if (startLaser != null) startLaser.Invoke();
+
+            _stopLaserAction = _emitter.FireUntilFurtherNotice();
+
+            Vector3 endDestination = _startingPoint.position;
+
+            if (_isInStartingPosition)
+            {
+                endDestination = _secondPoint.position;
+            }
+
+            StartCoroutine(MovementCoroutine(transform.position, endDestination));
+        }
+
+        private void StopAttack()
+        {
+            
+            if (_stopLaserAction != null) _stopLaserAction.Invoke();
+            if (stopLaser != null) stopLaser.Invoke();
+
+            _isInStartingPosition = !_isInStartingPosition;
+
+            StartCoroutine(StaticCoroutines.DoAfterDelay(_cooldown, StartAttack));
+        }
+
+        private IEnumerator MovementCoroutine(Vector3 startingPosition, Vector3 endPosition)
+        {
+            var t = 0f;
+
+            while (t <= 1)
+            {
+                t += WorldStateMachine.Instance.DeltaTime / _movementStats.MovementDelay;
+                yield return null;
+            }
+
+            t = 0f;
+
+            while (t <= 1)
+            {
+                transform.position = Vector3.LerpUnclamped(startingPosition, endPosition, _movementStats.MovementCurve.Evaluate(t));
+                t += WorldStateMachine.Instance.DeltaTime / _movementStats.MovementSpeed;
+                yield return null;
+            }
+
+            StopAttack();
+        }
+
+    }
+}
