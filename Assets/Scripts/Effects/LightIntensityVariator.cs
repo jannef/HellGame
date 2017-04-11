47266b7d3627360b1c3cc4a50150ec0eab0f0a93
Janne Forsell <jannef1337@gmail.com>
2017-04-12 02:38:03 +0300

    Comments
    +Comments on classes relating to sphere boss animation
    +Comments on fireplace light pulse

--- Assets/Scripts/Effects/LightIntensityVariator.cs
+++ Assets/Scripts/Effects/LightIntensityVariator.cs
@@ -4,16 +4,44 @@ using fi.tamk.hellgame.world;
 
 namespace fi.tamk.hellgame.effects
 {
+    /// <summary>
+    /// Variator for light source intensity.
+    /// </summary>
     public class LightIntensityVariator : MonoBehaviour
     {
+        /// <summary>
+        /// Pulsing curve for a the light intensity cycle.
+        /// </summary>
         [SerializeField] private AnimationCurve _curve;
+
+        /// <summary>
+        /// Cycöe duration.
+        /// </summary>
         [SerializeField] private float _cycleDuration;
+
+        /// <summary>
+        /// Magnitude delta that corresponds with 1.0f in the curve.
+        /// </summary>
         [SerializeField] private float _cycleMagnitude;
+
+        /// <summary>
+        /// Reference to the light.
+        /// </summary>
         private Light _light;
 
+        /// <summary>
+        /// Internal timer.
+        /// </summary>
         private float _timer = 0f;
+
+        /// <summary>
+        /// Original intensity value at startup.
+        /// </summary>
         private float _originalIntensity;
 
+        /// <summary>
+        /// Caches references, randomizes timer phase and stores original intensity.
+        /// </summary>
         private void Awake()
         {
             _light = GetComponent<Light>();
@@ -21,6 +49,9 @@ namespace fi.tamk.hellgame.effects
             _timer = Random.Range(0, _cycleDuration);
         }
 
+        /// <summary>
+        /// Update cycle, evaluates given curve in a loop.
+        /// </summary>
         private void Update()
         {
             _timer += WorldStateMachine.Instance.DeltaTime;
--- Assets/Scripts/Effects/SphereBoss/FloatingSpherePart.cs
+++ Assets/Scripts/Effects/SphereBoss/FloatingSpherePart.cs
@@ -5,28 +5,74 @@ using UnityEngine;
 
 namespace fi.tamk.hellgame.effects
 {
+    /// <summary>
+    /// Part of a shell on sphere boss.
+    /// </summary>
     public class FloatingSpherePart : MonoBehaviour
     {
+        /// <summary>
+        /// Normal direction of this shell part. Must be configured manually since all the shell parts
+        /// have same pivot location at origo of the boss.
+        /// </summary>
         [SerializeField] private Vector3 _normal;
+
+        /// <summary>
+        /// Rotation speed of the shell part along the normal.
+        /// </summary>
         public float RotationSpeed = 10f;
 
+        /// <summary>
+        /// Reference to rotation coroutine being ran.
+        /// </summary>
         private Coroutine _rotationTween;
+
+        /// <summary>
+        /// Reference to hovering coroutine being ran.
+        /// </summary>
         private Coroutine _hoverTween;
 
+        /// <summary>
+        /// Reference to convex collider on this shell piece.
+        /// </summary>
         private Collider _collider;
+
+        /// <summary>
+        /// Reference to rigidbody on this shell piece.
+        /// </summary>
         public Rigidbody Rigidbody;
 
+        /// <summary>
+        /// If this shell piece is blown off the boss.
+        /// </summary>
         private bool _isDestroyed = false;
+
+        /// <summary>
+        /// Timer to vanish into floor.
+        /// </summary>
         private float _vanishTimer = 0f;
+
+        /// <summary>
+        /// Vanishing coroutine running.
+        /// </summary>
         private Coroutine _vanishCoroutine;
 
-        public void Init(float initialRotation)
+        /// <summary>
+        /// Initializes script by setting references.
+        /// </summary>
+        /// <remarks>
+        /// Called from SpehereShellController.
+        /// </remarks>
+        /// <param name="initialRotation">Initial rotation speed to set.</param>
+        public void Init(float initialRotation = 0f)
         {
             RotationSpeed = initialRotation;
             _collider = GetComponent<Collider>();
             Rigidbody = GetComponent<Rigidbody>();
         }
 
+        /// <summary>
+        /// SphereShellController calls this when this needs to be realeased from it's parent.
+        /// </summary>
         public void ReleaseFromParent()
         {
             transform.parent = null;
@@ -36,6 +82,9 @@ namespace fi.tamk.hellgame.effects
             _isDestroyed = true;
         }
 
+        /// <summary>
+        /// Starts vanish coroutine.
+        /// </summary>
         private void Update()
         {
             if (!_isDestroyed)
@@ -52,17 +101,31 @@ namespace fi.tamk.hellgame.effects
             }
         }
 
+        /// <summary>
+        /// Rotates around _normal direction configured in Unity editor.
+        /// </summary>
         private void RotateAroundNormal()
         {
             transform.Rotate(_normal, RotationSpeed * WorldStateMachine.Instance.DeltaTime, Space.Self);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="newRotation"></param>
+        /// <param name="transitionDuration"></param>
         public void ChangeRotationSpeedOverTime(float newRotation, float transitionDuration)
         {
             if (_rotationTween != null) StopCoroutine(_rotationTween);
             _rotationTween = StartCoroutine(TweenRotationSpeed(newRotation, transitionDuration));
         }
 
+        /// <summary>
+        /// Starts hovering coroutine.
+        /// </summary>
+        /// <param name="height">height to hover to</param>
+        /// <param name="duration">How long from start to end should it take</param>
+        /// <param name="curve">Curve along which to animate</param>
         public void HoverAwayFromCore(float height, float duration, AnimationCurve curve)
         {
             if (_hoverTween == null)
@@ -71,6 +134,12 @@ namespace fi.tamk.hellgame.effects
             }
         }
 
+        /// <summary>
+        /// Vanishing coroutine to make this piece sink into floor after being blown off.
+        /// </summary>
+        /// <param name="duration">How long should it take to sink in and get Destroy() called.</param>
+        /// <param name="speed"></param>
+        /// <returns></returns>
         private IEnumerator VanishAfterDestroyed(float duration, Vector3 speed)
         {
             Rigidbody.isKinematic = true;
@@ -86,26 +155,37 @@ namespace fi.tamk.hellgame.effects
 
             Destroy(gameObject);
         }
-
-        private Vector3 _oldPosition;
+        /// <summary>
+        /// Hovering coroutine.
+        /// </summary>
+        /// <param name="height">height to hover to</param>
+        /// <param name="duration">How long from start to end should it take</param>
+        /// <param name="curve">Curve along which to animate</param>
+        /// <returns>Not used.</returns>
         private IEnumerator HoverInNormalDirection(float height, float duration, AnimationCurve curve)
         {
             if (!(duration >= 0.1f)) yield break;
 
             var timer = 0f;
-            _oldPosition = transform.localPosition;
-            var newPosition = _oldPosition + _normal * height;
+            var oldPosition = transform.localPosition;
+            var newPosition = oldPosition + _normal * height;
             while (timer < duration)
             {
                 timer += WorldStateMachine.Instance.DeltaTime;
-                transform.localPosition = Vector3.Lerp(_oldPosition, newPosition, curve.Evaluate(timer / duration));
+                transform.localPosition = Vector3.Lerp(oldPosition, newPosition, curve.Evaluate(timer / duration));
                 yield return null;
             }
 
-            transform.localPosition = _oldPosition; // Just to be sure
+            transform.localPosition = oldPosition; // Just to be sure
             _hoverTween = null;
         }
 
+        /// <summary>
+        /// Rotation speed steen towards new rotation speed.
+        /// </summary>
+        /// <param name="newSpeed">Value to tween into</param>
+        /// <param name="duration">How long should the tween take.</param>
+        /// <returns></returns>
         private IEnumerator TweenRotationSpeed(float newSpeed, float duration)
         {
             if (!(duration >= 0.1f)) yield break;
