b15958a4d01570dfae8938a3622871359fd46091
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-04-13 14:27:05 +0300

    * Changed Cellar_Boss entrance to the boss emerging from a puddle

---
+++ Assets/Scripts/Effects/LightIntensityINterpolationEffect.cs
@@ -0,0 +1,40 @@
+﻿using fi.tamk.hellgame.world;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.effect
+{
+
+    public class LightIntensityINterpolationEffect : MonoBehaviour
+    {
+        [SerializeField]
+        private float _effectLenght;
+        [SerializeField]
+        private AnimationCurve _lerpCurve;
+        private Light _light;
+        [SerializeField] private float startIntensity;
+        [SerializeField] private float endIntensity;
+
+        public void StartEffect()
+        {
+            _light = GetComponent<Light>();
+            if (_light == null) return;
+            StartCoroutine(ChangeLightIntensityRoutine());
+        }
+
+        private IEnumerator ChangeLightIntensityRoutine()
+        {
+            var t = 0f;
+
+            while (t <= _effectLenght)
+            {
+                t += WorldStateMachine.Instance.DeltaTime;
+                _light.intensity = Mathf.Lerp(startIntensity, endIntensity, _lerpCurve.Evaluate(t / _effectLenght));
+                yield return null;
+            }
+
+            _light.intensity = endIntensity;
+        }
+    }
+}
---
+++ Assets/Scripts/Effects/SizeChangeEffect.cs
@@ -0,0 +1,44 @@
+﻿using fi.tamk.hellgame.world;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.effect
+{
+
+    public class SizeChangeEffect : MonoBehaviour
+    {
+        [SerializeField] private AnimationCurve _sizeLerp;
+        [SerializeField] private float _changeDuration;
+        [SerializeField] private Vector3 _finalSize;
+        [SerializeField] private Transform _remoteChild;
+        public bool DestroyAfterEffect = false;
+
+        public void StartChangingSize()
+        {
+            //if (_remoteChild == null) throw new UnityException("SizeChangeEffect _remoteChild variable not set. Track Blob boss transform into the serialized field");
+            StartCoroutine(SizeChangeCoroutine());
+        }
+
+        private IEnumerator SizeChangeCoroutine()
+        {
+            var t = 0f;
+            var originalScale = transform.localScale;
+
+            while (t <= _changeDuration)
+            {
+                t += WorldStateMachine.Instance.DeltaTime;
+                var ratio = t / _changeDuration; 
+                transform.localScale = Vector3.Lerp(originalScale, _finalSize, _sizeLerp.Evaluate(ratio));
+                yield return null;
+            }
+
+            transform.localScale = _finalSize;
+            if (DestroyAfterEffect)
+            {
+                Destroy(gameObject);
+            }
+            yield return null;
+        }
+    }
+}
