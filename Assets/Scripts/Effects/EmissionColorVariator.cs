f6a648f3b0b83b19300fd6b2aec418a677cf6670
Janne Forsell <jannef1337@gmail.com>
2017-04-15 03:59:05 +0300

    EmissionVariator
    +Added a script that cycles emissions of a renderer. It's pretty expensive, so use caution when slapping it on something that has many instances...
    +Added the variator to pickups. This might be a risk to framerates.

---
+++ Assets/Scripts/Effects/EmissionColorVariator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using fi.tamk.hellgame.utils;
+using fi.tamk.hellgame.world;
+
+namespace fi.tamk.hellgame.effects
+{
+    public class EmissionColorVariator : MonoBehaviour
+    {
+        [SerializeField] private Color _highEmission;
+        [SerializeField] private Color _lowEmission;
+        [SerializeField] private AnimationCurve _emissionCurve;
+        [SerializeField] private float _emissionInterval;
+
+        private Renderer _renderer;
+        private float _timer = 0f;
+
+        private static bool CalledOne = false;
+        private void Awake()
+        {
+            _renderer = GetComponentInChildren<Renderer>();
+        }
+
+        private void Update()
+        {
+            CalledOne = true;
+            _timer += WorldStateMachine.Instance.DeltaTime;
+
+            if (_timer > _emissionInterval) _timer -= _emissionInterval;
+            var ratio = _timer / _emissionInterval;
+
+            _renderer.material.SetColor("_EmissionColor", Color.Lerp(_lowEmission, _highEmission, _emissionCurve.Evaluate(ratio)));
+        }
+    }
+}
