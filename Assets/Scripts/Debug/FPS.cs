b96aab33a52127ed2aa8bb0aecac97e12cee95ea
Janne Forsell <jannef1337@gmail.com>
2017-04-15 04:47:45 +0300

    Fps counter
    + Added a fucntioning FPS counter to GameplayCanvas
    + Calculates fps as a rolling average for last 150 frames.

--- Assets/Scripts/Debug/FPS.cs
+++ Assets/Scripts/Debug/FPS.cs
@@ -4,15 +4,36 @@ using fi.tamk.hellgame.world;
 using UnityEngine.UI;
 using UnityEngine;
 
-public class FPS : MonoBehaviour {
-    Text _text;
-	// Use this for initialization
-	void Start () {
-        _text = GetComponent<Text>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        _text.text = (1f / WorldStateMachine.Instance.DeltaTime).ToString();
-	}
+namespace fi.tamk.hellgame.ui
+{
+    public class FPS : MonoBehaviour
+    {
+        private const int Samples = 150;
+        private readonly float[] _deltaArr = new float[Samples];
+        private int index = 0;
+
+        Text _text;
+
+        private void Start()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        private void Update()
+        {
+            _deltaArr[index] = Time.unscaledDeltaTime;
+            index = (index + 1) % Samples;
+            if (index % 5 == 0) _text.text = string.Format("{0:000}", CalculateFps());
+        }
+
+        private float CalculateFps()
+        {
+            var sum = 0f;
+            for (var i = 0; i < Samples; i++)
+            {
+                sum += _deltaArr[i];
+            }
+            return Samples / sum;
+        }
+    }
 }
