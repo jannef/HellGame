86d07b6a90c051f1b47c0c1c1cf5d661ec7df2d5
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-04-19 16:28:30 +0300

    + Script that can be attached to ui text to easily link it to a localized string

---
+++ Assets/Scripts/UI/UiLocalizedTextSetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.ui
+{
+
+    public class UiLocalizedTextSetter : MonoBehaviour {
+        [SerializeField] private LocaleStrings.StringsEnum _textToUse;
+
+        // Use this for initialization
+        void Start() {
+            TextMeshProUGUI textMesh = GetComponent<TextMeshProUGUI>();
+
+            if (textMesh != null)
+            {
+                textMesh.text = LocaleStrings.LocalizedStringFromEnum(_textToUse);
+            }
+        }
+    }
+}
