09a570641a78b95bebc1fe632d37ed768f3311f5
Janne Forsell <jannef1337@gmail.com>
2017-04-23 22:34:37 +0300

    Wall boss health bar rework
    +Health bar internal works now MUCH simpler
    +Health bar now goes out all the way during boss life
    +Health bar lights now have random priority to light up and go out, instead of hand configured.
    +Health bar now supports arbitrary number of indicator lights.

--- Assets/Scripts/World/BossDamageThresholdEffectMaster.cs
+++ Assets/Scripts/World/BossDamageThresholdEffectMaster.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace fi.tamk.hellgame.world
@@ -9,30 +10,55 @@ namespace fi.tamk.hellgame.world
 
     public class BossDamageThresholdEffectMaster : MonoBehaviour
     {
-        public event Action<float> DamageEvent;
-        public event Action WakeUpEvent;
         [SerializeField] private HealthComponent _bossSystemToFollow;
+        [SerializeField] private float _wakeDelay = 0.11f;
 
-        private void Start()
+        private BossDamageThresholdEffect[] _children;
+        private int _popIterator = 0;
+
+        private float _threshold;
+        private float _nextPop;
+
+        private void Awake()
         {
             if (_bossSystemToFollow != null) _bossSystemToFollow.HealthChangeEvent += TakeDamage;
-            _bossSystemToFollow.DeathEffect.AddListener(DeathEffect);
+
+            _children = GetComponentsInChildren<BossDamageThresholdEffect>();
+            foreach (var c in _children)
+            {
+                c.Initialize();
+            }
+
+            _children = _children.OrderBy(x => x.Priority).ToArray();
+            _threshold = 1f / (_children.Length + 1);
+            _nextPop = 1f - _threshold;
         }
 
         public void WakeUp()
         {
-            if (WakeUpEvent != null) WakeUpEvent.Invoke();
+            StartCoroutine(WakeCycle());
         }
 
-        private void DeathEffect()
+        private IEnumerator WakeCycle()
         {
-            TakeDamage(0f, 0, 0);
+            foreach (var t in _children)
+            {
+                var timer = 0f;
+                while (timer < _wakeDelay)
+                {
+                    timer += WorldStateMachine.Instance.DeltaTime;
+                    yield return null;
+                }
+                t.Wake();
+            }
         }
 
         // Use this for initialization
-        void TakeDamage(float percentage, int hp, int max)
+        private void TakeDamage(float percentage, int hp, int max)
         {
-            if (DamageEvent != null) DamageEvent.Invoke(percentage);
+            if (!(percentage <= _nextPop)) return;
+            _nextPop -= _threshold;
+            _children[_popIterator++].Pop();
         }
     }
 }
