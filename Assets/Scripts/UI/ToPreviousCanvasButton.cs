deb5ebe5c303a910926dacb92e2cce410a2610a8
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-04-23 19:35:45 +0300

    Return buttons to pause menu and sound effect to exploding doors

---
+++ Assets/Scripts/UI/ToPreviousCanvasButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace fi.tamk.hellgame.ui
+{
+
+    public class ToPreviousCanvasButton : BasicMenuButton
+    {
+        private MenuCanvas _parentMenu;
+
+        void Start()
+        {
+            _parentMenu = GetComponentInParent<MenuCanvas>();
+        }
+
+        public override Action ClickThis(MenuCommander commander)
+        {
+            if (_parentMenu != null) _parentMenu.ReturnToPreviousCanvas(commander);
+            return null;
+        }
+
+        public override void Submit(MenuCommander commander)
+        {
+            if (_parentMenu != null) _parentMenu.ReturnToPreviousCanvas(commander);
+        }
+
+        public override void Activate()
+        {
+        }
+    }
+}
