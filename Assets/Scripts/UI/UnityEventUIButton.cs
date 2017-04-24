d070c88c39a7bea3a07d84af11bc88312d1d9e64
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-04-24 12:29:39 +0300

    Credits pop-up to pause menu
    * Has placeholder names

---
+++ Assets/Scripts/UI/UnityEventUIButton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace fi.tamk.hellgame.ui
+{
+
+    public class UnityEventUIButton : BasicMenuButton {
+        public UnityEvent SubmitEvent;
+        public UnityEvent PointerMovedToThisEvent;
+        public UnityEvent PonterModedFromThisEvent;
+
+        public override Action ClickThis(MenuCommander commander)
+        {
+            if (SubmitEvent != null) SubmitEvent.Invoke();
+            return base.ClickThis(commander);
+        }
+
+        public override void Submit(MenuCommander commander)
+        {
+            if (SubmitEvent != null) SubmitEvent.Invoke();
+            base.Submit(commander);
+        }
+
+        public override void MovePointerToThis(MenuCommander commander)
+        {
+            if (PointerMovedToThisEvent != null) PointerMovedToThisEvent.Invoke();
+            base.MovePointerToThis(commander);
+        }
+
+        public override void MovePointerFromThis()
+        {
+            if (PonterModedFromThisEvent != null) PonterModedFromThisEvent.Invoke();
+            base.MovePointerFromThis();
+        }
+    }
+}
