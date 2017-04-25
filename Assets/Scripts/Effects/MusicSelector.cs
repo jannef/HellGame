2509af21374057cb8b2cc1f9dec0ea83f2911e23
Janne Forsell <jannef1337@gmail.com>
2017-04-25 05:12:49 +0300

    Music selector
    +Music selector component, added to RoomIdentifier
    +Usage: drag to UnityEvent, call SwapToTrack(int) with correct parameters to easily switch into looping specified track,
    +Please do not make another "system" for this :)

---
+++ Assets/Scripts/Effects/MusicSelector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace fi.tamk.hellgame.effects
+{
+    public class MusicSelector : MonoBehaviour
+    {
+        private static MusicPlayer Music
+        {
+            get
+            {
+                // ReSharper disable once ConvertIfStatementToNullCoalescingExpression
+                // Unity things.
+                if (_music == null)
+                {
+                    _music = FindObjectOfType<MusicPlayer>();
+                }
+                return _music;
+            }
+        }
+        private static MusicPlayer _music;
+
+        private void SwapToTrack(int trackZeroBaseIndex)
+        {
+            Music.LoopTrack = true;
+            Music.PlayTrackByIndex(trackZeroBaseIndex);
+        }
+    }
+}
