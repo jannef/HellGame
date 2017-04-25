5ed54a36e7495289f6aea8561ae88829ff4f0d5c
ReinoH <reino.hamalainen@cs.tamk.fi>
2017-04-25 11:11:18 +0300

    Made kitchen_Boss encounter only begin after player shoots the boss first

---
+++ Assets/Scripts/World/EventBasedEncounterBeginner.cs
@@ -0,0 +1,21 @@
+﻿using fi.tamk.hellgame.world;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventBasedEncounterBeginner : MonoBehaviour {
+
+    private bool _hasBeenActivated = false;
+
+    public void BeginEncounter()
+    {
+        if (!_hasBeenActivated)
+        {
+            RoomIdentifier.BeginEncounter();
+            _hasBeenActivated = true;
+        }
+        
+    }
+
+
+}
