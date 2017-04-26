0305ce53cc4e11a5d70988fd383b9ad8b0ad68f2
Janne Forsell <jannef1337@gmail.com>
2017-04-26 16:01:44 +0300

    Reverted changes to localization and reapplied
    *Reverted changed that broke all the localization enums set in the project

--- Assets/Scripts/UI/LocaleStrings.cs
+++ Assets/Scripts/UI/LocaleStrings.cs
@@ -12,15 +12,14 @@ public static partial class LocaleStrings
 		"Completion Time",
 		"Hits Taken",
 		"Final Clear Time",
-		"Next Rank\n{0}",
+		"Next rank\n{0}",
 		"Automatoned Diner",
 		"Olde Columns",
 		"Vanilla Room",
 		"The Iron Chef",
 		"Apprentice's Cellar",
-		"Column Storage",
 		"Wet Treasures",
-		"The Puddler",
+		"Slime of Death",
 		"Dangerous Library",
 		"Laser Danger",
 		"Book Burnery",
@@ -47,6 +46,7 @@ public static partial class LocaleStrings
 		"Ultimate power!",
 		"Return",
 		"Press any key!",
+		"Column Storage",
 		""
 	};
 
@@ -62,9 +62,8 @@ public static partial class LocaleStrings
 		"Perushuone",
 		"Iron Chef",
 		"Oppipojan kellari",
-		"Pilarivarasto",
 		"Sitä märkää",
-		"Läikyttäjä",
+		"Kuoleman Lima",
 		"Vaarojen Kirjasto",
 		"Laservaara",
 		"Kirjapolttamo",
@@ -91,6 +90,7 @@ public static partial class LocaleStrings
 		"VOIMAAAAAA!",
 		"Palaa",
 		"Paina nappulaa :P",
+		"Pilarivarasto",
 		""
 	};
 
@@ -116,63 +116,63 @@ public static partial class LocaleStrings
 
 	public static string UI_ROOM_CELLAR_1 { get { return CurrentLocale[10]; } }
 
-	public static string UI_ROOM_CELLAR_3 { get { return CurrentLocale[11]; } }
+	public static string UI_ROOM_CELLAR_2 { get { return CurrentLocale[11]; } }
 
-	public static string UI_ROOM_CELLAR_2 { get { return CurrentLocale[12]; } }
+	public static string UI_ROOM_CELLAR_BOSS { get { return CurrentLocale[12]; } }
 
-	public static string UI_ROOM_CELLAR_BOSS { get { return CurrentLocale[13]; } }
+	public static string UI_ROOM_LIBRARY_1 { get { return CurrentLocale[13]; } }
 
-	public static string UI_ROOM_LIBRARY_1 { get { return CurrentLocale[14]; } }
+	public static string UI_ROOM_LIBRARY_2 { get { return CurrentLocale[14]; } }
 
-	public static string UI_ROOM_LIBRARY_2 { get { return CurrentLocale[15]; } }
+	public static string UI_ROOM_LIBRARY_3 { get { return CurrentLocale[15]; } }
 
-	public static string UI_ROOM_LIBRARY_3 { get { return CurrentLocale[16]; } }
+	public static string UI_ROOM_LIBRARY_BOSS { get { return CurrentLocale[16]; } }
 
-	public static string UI_ROOM_LIBRARY_BOSS { get { return CurrentLocale[17]; } }
+	public static string UI_ROOM_CHAMBERS_BOSS { get { return CurrentLocale[17]; } }
 
-	public static string UI_ROOM_CHAMBERS_BOSS { get { return CurrentLocale[18]; } }
+	public static string UI_ROOM_LEVELSELECTHUB { get { return CurrentLocale[18]; } }
 
-	public static string UI_ROOM_LEVELSELECTHUB { get { return CurrentLocale[19]; } }
+	public static string UI_MENU_RETRY { get { return CurrentLocale[19]; } }
 
-	public static string UI_MENU_RETRY { get { return CurrentLocale[20]; } }
+	public static string UI_HIGHSCORE_TOTALTIME { get { return CurrentLocale[20]; } }
 
-	public static string UI_HIGHSCORE_TOTALTIME { get { return CurrentLocale[21]; } }
+	public static string UI_HIGHSCORE_COMPLETIONPERCENT { get { return CurrentLocale[21]; } }
 
-	public static string UI_HIGHSCORE_COMPLETIONPERCENT { get { return CurrentLocale[22]; } }
+	public static string UI_MENU_CONTINUE { get { return CurrentLocale[22]; } }
 
-	public static string UI_MENU_CONTINUE { get { return CurrentLocale[23]; } }
+	public static string UI_MENU_PAUSE { get { return CurrentLocale[23]; } }
 
-	public static string UI_MENU_PAUSE { get { return CurrentLocale[24]; } }
+	public static string UI_MENU_FIRE { get { return CurrentLocale[24]; } }
 
-	public static string UI_MENU_FIRE { get { return CurrentLocale[25]; } }
+	public static string UI_MENU_DASH { get { return CurrentLocale[25]; } }
 
-	public static string UI_MENU_DASH { get { return CurrentLocale[26]; } }
+	public static string UI_MENU_SOUND { get { return CurrentLocale[26]; } }
 
-	public static string UI_MENU_SOUND { get { return CurrentLocale[27]; } }
+	public static string UI_MENU_OPTIONS { get { return CurrentLocale[27]; } }
 
-	public static string UI_MENU_OPTIONS { get { return CurrentLocale[28]; } }
+	public static string UI_MENU_GAMEPAD { get { return CurrentLocale[28]; } }
 
-	public static string UI_MENU_GAMEPAD { get { return CurrentLocale[29]; } }
+	public static string UI_MENU_KEYBOARD { get { return CurrentLocale[29]; } }
 
-	public static string UI_MENU_KEYBOARD { get { return CurrentLocale[30]; } }
+	public static string UI_MENU_SFX { get { return CurrentLocale[30]; } }
 
-	public static string UI_MENU_SFX { get { return CurrentLocale[31]; } }
+	public static string UI_MENU_MUSIC { get { return CurrentLocale[31]; } }
 
-	public static string UI_MENU_MUSIC { get { return CurrentLocale[32]; } }
+	public static string UI_MENU_QUIT { get { return CurrentLocale[32]; } }
 
-	public static string UI_MENU_QUIT { get { return CurrentLocale[33]; } }
+	public static string UI_MENU_LEVELSELECT { get { return CurrentLocale[33]; } }
 
-	public static string UI_MENU_LEVELSELECT { get { return CurrentLocale[34]; } }
+	public static string UI_MENU_GAMEOVER { get { return CurrentLocale[34]; } }
 
-	public static string UI_MENU_GAMEOVER { get { return CurrentLocale[35]; } }
+	public static string UI_MENU_CREDITS { get { return CurrentLocale[35]; } }
 
-	public static string UI_MENU_CREDITS { get { return CurrentLocale[36]; } }
+	public static string UI_MENU_LIMIT { get { return CurrentLocale[36]; } }
 
-	public static string UI_MENU_LIMIT { get { return CurrentLocale[37]; } }
+	public static string UI_MENU_RETURN { get { return CurrentLocale[37]; } }
 
-	public static string UI_MENU_RETURN { get { return CurrentLocale[38]; } }
+	public static string UI_MENU_ANYKEY { get { return CurrentLocale[38]; } }
 
-	public static string UI_MENU_ANYKEY { get { return CurrentLocale[39]; } }
+	public static string UI_ROOM_CELLAR_3 { get { return CurrentLocale[39]; } }
 
 	public enum StringsEnum : int
 	{
@@ -187,34 +187,34 @@ public static partial class LocaleStrings
 		UI_ROOM_KITCHEN_3=8,
 		UI_ROOM_KITCHEN_BOSS=9,
 		UI_ROOM_CELLAR_1=10,
-		UI_ROOM_CELLAR_3=11,
-		UI_ROOM_CELLAR_2=12,
-		UI_ROOM_CELLAR_BOSS=13,
-		UI_ROOM_LIBRARY_1=14,
-		UI_ROOM_LIBRARY_2=15,
-		UI_ROOM_LIBRARY_3=16,
-		UI_ROOM_LIBRARY_BOSS=17,
-		UI_ROOM_CHAMBERS_BOSS=18,
-		UI_ROOM_LEVELSELECTHUB=19,
-		UI_MENU_RETRY=20,
-		UI_HIGHSCORE_TOTALTIME=21,
-		UI_HIGHSCORE_COMPLETIONPERCENT=22,
-		UI_MENU_CONTINUE=23,
-		UI_MENU_PAUSE=24,
-		UI_MENU_FIRE=25,
-		UI_MENU_DASH=26,
-		UI_MENU_SOUND=27,
-		UI_MENU_OPTIONS=28,
-		UI_MENU_GAMEPAD=29,
-		UI_MENU_KEYBOARD=30,
-		UI_MENU_SFX=31,
-		UI_MENU_MUSIC=32,
-		UI_MENU_QUIT=33,
-		UI_MENU_LEVELSELECT=34,
-		UI_MENU_GAMEOVER=35,
-		UI_MENU_CREDITS=36,
-		UI_MENU_LIMIT=37,
-		UI_MENU_RETURN=38,
-		UI_MENU_ANYKEY=39,
+		UI_ROOM_CELLAR_2=11,
+		UI_ROOM_CELLAR_BOSS=12,
+		UI_ROOM_LIBRARY_1=13,
+		UI_ROOM_LIBRARY_2=14,
+		UI_ROOM_LIBRARY_3=15,
+		UI_ROOM_LIBRARY_BOSS=16,
+		UI_ROOM_CHAMBERS_BOSS=17,
+		UI_ROOM_LEVELSELECTHUB=18,
+		UI_MENU_RETRY=19,
+		UI_HIGHSCORE_TOTALTIME=20,
+		UI_HIGHSCORE_COMPLETIONPERCENT=21,
+		UI_MENU_CONTINUE=22,
+		UI_MENU_PAUSE=23,
+		UI_MENU_FIRE=24,
+		UI_MENU_DASH=25,
+		UI_MENU_SOUND=26,
+		UI_MENU_OPTIONS=27,
+		UI_MENU_GAMEPAD=28,
+		UI_MENU_KEYBOARD=29,
+		UI_MENU_SFX=30,
+		UI_MENU_MUSIC=31,
+		UI_MENU_QUIT=32,
+		UI_MENU_LEVELSELECT=33,
+		UI_MENU_GAMEOVER=34,
+		UI_MENU_CREDITS=35,
+		UI_MENU_LIMIT=36,
+		UI_MENU_RETURN=37,
+		UI_MENU_ANYKEY=38,
+		UI_ROOM_CELLAR_3=39,
 	}
 }
