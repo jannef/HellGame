9f33a0d9e8cf1e72fc7f400a449e159cd4e3afbb
Janne Forsell <jannef1337@gmail.com>
2017-04-29 18:20:41 +0300

    Sawblade trap
    +Terrible sawblade trap of death with placeholder assets
    +Debug event firer

---
+++ Assets/Scripts/Utils/DebugEventFirer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace tamk.fi.hellgame.utils.debug
+{
+    public delegate bool GetInput(KeyCode mode);
+
+    public static class Debug
+    {
+        public static GetInput GetInputFromInputModes(InputModes whichMode)
+        {
+            switch (whichMode)
+            {
+                case InputModes.Key:
+                    return Input.GetKey;
+                case InputModes.KeyDown:
+                    return Input.GetKeyDown;
+                case InputModes.KeyUp:
+                    return Input.GetKeyUp;
+                default:
+                    throw new ArgumentOutOfRangeException("whichMode", whichMode, null);
+            }
+        }
+    }
+
+    [Serializable]
+    public struct DebugKeyEventPair
+    {
+        [SerializeField] public UnityEvent Event;
+        [SerializeField] public KeyCode Key;
+        [SerializeField] public InputModes Mode;
+    }
+
+    public enum InputModes
+    {
+        Key = 0,
+        KeyDown = 1,
+        KeyUp = 2
+    }
+
+    public class DebugEventFirer : MonoBehaviour
+    {
+        [SerializeField] public DebugKeyEventPair[] Events;
+
+        private void Update()
+        {
+            foreach (var @event in Events)
+            {
+                var d = Debug.GetInputFromInputModes(@event.Mode);
+                if (d.Invoke(@event.Key))
+                {
+                    @event.Event.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/World/TrapSensor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+namespace fi.tamk.hellgame.world
+{
+    public class TrapSensor : MonoBehaviour
+    {
+        public UnityEvent OnTrapTrigger;
+        [SerializeField, Range(0f, 60f)] protected float Cooldown;
+
+        private float _timer;
+
+        private void Update()
+        {
+            _timer -= WorldStateMachine.Instance.DeltaTime;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (_timer <= 0f && OnTrapTrigger != null)
+            {
+                OnTrapTrigger.Invoke();
+                _timer = Cooldown;
+            }
+        }
+    }
+}
