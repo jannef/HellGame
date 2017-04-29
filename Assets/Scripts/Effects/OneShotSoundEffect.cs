aac04f619944c564ac7d6e2a2b3254fb41c83913
Janne Forsell <jannef1337@gmail.com>
2017-04-30 00:09:01 +0300

    One shot sound feature
    +Can now play random from the array

--- Assets/Scripts/Effects/OneShotSoundEffect.cs
+++ Assets/Scripts/Effects/OneShotSoundEffect.cs
@@ -1,6 +1,7 @@
 ﻿using FMODUnity;
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace fi.tamk.hellgame.effects
 {
@@ -26,6 +27,13 @@ namespace fi.tamk.hellgame.effects
                 throw new UnityException(string.Format("Parameters out of range [0, {0}]: {1}", EventArray.Length - 1, index));
             }
 
+            if (String.IsNullOrEmpty(EventArray[index])) return;
+            RuntimeManager.PlayOneShot(EventArray[index], transform.position);
+        }
+
+        public void PlayRandomFromArray()
+        {
+            var index = Random.Range(0, EventArray.Length - 1);
             if (String.IsNullOrEmpty(EventArray[index])) return;
             RuntimeManager.PlayOneShot(EventArray[index], transform.position);
         }
