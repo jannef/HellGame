b8805995ce3bc25bd2e8dfb260d95bb97b0fde16
Janne Forsell <jannef1337@gmail.com>
2017-04-30 15:02:35 +0300

    Beholder wanderign explosion
    +Fixed by changing simulation space to local

---
+++ Assets/Scripts/Utils/GameObjectDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace fi.tamk.hellgame.utils
+{
+    public class GameObjectDestroyer : MonoBehaviour
+    {
+        public void DestroyGameObject(bool immeadite)
+        {
+            if (immeadite)
+            {
+                DestroyImmediate(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
