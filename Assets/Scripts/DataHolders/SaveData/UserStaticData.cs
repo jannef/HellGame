dc14ec74ef583c7e3817e1b9994d8affad3eb482
Reino <reino.hamalainen@cs.tamk.fi>
2017-05-03 13:41:31 +0300

    Added gameplay to Chambers_2

--- Assets/Scripts/DataHolders/SaveData/UserStaticData.cs
+++ Assets/Scripts/DataHolders/SaveData/UserStaticData.cs
@@ -51,7 +51,8 @@ namespace fi.tamk.hellgame.dataholders
                 {LocaleStrings.StringsEnum.UI_ROOM_KITCHEN_LEVELSELECT, 17 },
                 {LocaleStrings.StringsEnum.UI_ROOM_LIBRARY_LEVELSELECT, 18 },
                 {LocaleStrings.StringsEnum.UI_ROOM_HEROIC_BELURKER, 19 },
-                {LocaleStrings.StringsEnum.UI_CREDIT_DEV, 20 }
+                {LocaleStrings.StringsEnum.UI_CREDIT_DEV, 20 },
+                {LocaleStrings.StringsEnum.RANK_S, 21 }
             };
 
             LoadData();
