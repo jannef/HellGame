82c748cb1d7c16a64e44b257990104da4d36d806
Reino <reino.hamalainen@cs.tamk.fi>
2017-05-09 13:47:27 +0300

    Made first time transition a bit better

--- Assets/Scripts/UI/TitleScreenSceneTransition.cs
+++ Assets/Scripts/UI/TitleScreenSceneTransition.cs
@@ -2,7 +2,9 @@
 using fi.tamk.hellgame.world;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace fi.tamk.hellgame.ui
 {
@@ -13,12 +15,15 @@ namespace fi.tamk.hellgame.ui
         [SerializeField] private float _autosaveCanvasFadeInLenght;
         [SerializeField] private AnimationCurve _autosaveFadeInCurve;
         [SerializeField] private float _autosaveReminderLenght;
+        [SerializeField] private TextMeshProUGUI _autosaveReminderText;
+        [SerializeField] private float _autosaveReminderTextFadeInLenght;
+        [SerializeField] private AnimationCurve _autosaveReminderTextCurve;
 
         public override void StartSceneTransition(int sceneToLoad)
         {
             var SaveGame = UserStaticData.GetRoomData((int) LegalScenes.Cellar_1);
 
-            if (SaveGame == null)
+            if (SaveGame == null && sceneToLoad != SceneManager.GetActiveScene().buildIndex)
             {
                 StartCoroutine(AutoSaveReminderRoutine(sceneToLoad));
                 return;
@@ -29,6 +34,7 @@ namespace fi.tamk.hellgame.ui
 
         IEnumerator AutoSaveReminderRoutine(int sceneToLoad)
         {
+            _autosaveReminderText.alpha = 0;
             var t = 0f;
             while (t < 1)
             {
@@ -39,6 +45,16 @@ namespace fi.tamk.hellgame.ui
             }
 
             _autosaveCanvas.alpha = _autosaveFadeInCurve.Evaluate(1);
+
+            t = 0f;
+            while (t < 1)
+            {
+                t += Time.unscaledDeltaTime / _autosaveReminderTextFadeInLenght;
+                _autosaveReminderText.alpha = _autosaveFadeInCurve.Evaluate(t);
+                yield return null;
+            }
+
+            _autosaveReminderText.alpha = _autosaveFadeInCurve.Evaluate(1);
 
             t = 0f;
             while (t < 1)
